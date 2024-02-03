0958c62ae37dc9c4a06aa2f5b4b257a8faca54f8
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-03 22:47:53 +0530

    Refactor post_details function to include slug parameter

--- blog/app/urls.py
+++ blog/app/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("posts", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post_details, name="post-detail-page"),  # type: ignore
+    path("posts/<slug:slug>", views.post_details, name="post-detail-page"),
 ]
--- blog/app/views.py
+++ blog/app/views.py
@@ -9,5 +9,5 @@ def posts(request):
     return render(request, "app/all-posts.html")
 
 
-def post_details(request):
-    pass
+def post_details(request, slug):
+    return render(request, "app/post-detail.html")
